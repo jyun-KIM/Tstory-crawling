71a5e6131a6147f27117865608f08396d3a5cf87
jyun27 <kimjyun27@gmail.com>
2024-08-30 17:33:56 +0900

    chore: 주석 수정

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://k-jyun.tistory.com/'  # 실제 크롤링하려는 URL로 변경
+base_url = 'https://k-jyun.tistory.com/' # 실제 크롤링하려는 URL로 변경
 
-# 크롤링할 페이지 범위 설정 (예: 1~5 페이지)
+# url의 headers를 알아내는 코드 
+#print(base_url.request.headers)
+
+# 크롤링할 페이지 범위 설정 > 해당 코드는 1~8 페이지 탐색 중 
 for page_number in range(1, 9):
-    url = f'{base_url}?page={page_number}'  # 페이지 번호를 URL에 삽입
-    headers = {'User-Agent': 'python-requests/2.32.3', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
+    url = f'{base_url}?page={page_number}' # 페이지 번호를 URL에 삽입
+    headers = {'User-Agent': 'python-requests/2.32.3', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'} # 크롤링 하려는 url의 headers로 변경 
     response = requests.get(url, headers=headers)
     
     soup = BeautifulSoup(response.text, 'html.parser')
